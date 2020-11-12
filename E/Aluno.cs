8983f9874017174ff43f0c6c0965d9be9da73fe4
Andrey Souza <andreyajj@hotmail.com>
2020-11-12 01:11:25 -0300

    Calculo das notas adicionado ao projeto escola

---
+++ E/Aluno.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace E
+{
+    class Aluno
+    {
+        public string Nome { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+
+        public Aluno (string nome, double nota1, double nota2, double nota3)
+        {
+            Nome = nome;
+            Nota1 = nota1;
+            Nota2 = nota2;
+            Nota3 = nota2;
+        }
+
+        public double NotaFinal()
+        {
+            return Nota1 + Nota2 + Nota3 / 3.0;
+        }
+    }
+}
